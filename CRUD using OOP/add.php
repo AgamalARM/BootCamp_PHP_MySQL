41240df5784afefd0a5b7af1256f72bb2bdff644
AgamalARM <agamalarm@gmail.com>
2024-06-20 23:20:50 +0300

    crud using oop

---
+++ CRUD using OOP/add.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h2 class="p-3 col text-center mt-5 text-white bg-primary">Add Employee</h2>
+        </div>
+
+    </div>
+
+
+</div>
